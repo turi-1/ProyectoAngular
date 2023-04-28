deebb614aed18ee1cd6245ea8e41b64363f55c0e
carlostorres <carlostorresj1803@gmail.com>
2023-04-28 14:50:32 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { LoginComponent } from './pagina/login/login.component';
+import { RegistroComponent } from './pagina/registro/registro.component';
+import { AdminComponent } from './pagina/admin/admin.component';
+import { VentasComponent } from './pagina/ventas/ventas.component';
+import { ProductosComponent } from './pagina/productos/productos.component';
+import { ChatComponent } from './pagina/chat/chat.component';
+import { UsuariosComponent } from './pagina/usuarios/usuarios.component';
+import { FavoritosComponent } from './pagina/favoritos/favoritos.component';
+import { ComprasComponent } from './pagina/compras/compras.component';
+import { ComentariosComponent } from './pagina/comentarios/comentarios.component';
+import { TransaccionComponent } from './pagina/transaccion/transaccion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    RegistroComponent,
+    AdminComponent,
+    VentasComponent,
+    ProductosComponent,
+    ChatComponent,
+    UsuariosComponent,
+    FavoritosComponent,
+    ComprasComponent,
+    ComentariosComponent,
+    TransaccionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pagina/transaccion/transaccion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaccion',
+  templateUrl: './transaccion.component.html',
+  styleUrls: ['./transaccion.component.css']
+})
+export class TransaccionComponent {
+
+}
